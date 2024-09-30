0948a88d67213ed27b729f806ec1e0cafe702341
gilangherdiansyah80 <gilangherdiansyah404@gmail.com>
2024-10-01 01:08:07 +0700

    make responsive website for tablet and dekstop

--- src/components/Fragments/HelpSection.jsx
+++ src/components/Fragments/HelpSection.jsx
@@ -14,51 +14,56 @@ const HelpSection = () => {
 
     return (
         <Fragment>
-            <section className="flex flex-col gap-y-3">
-                <h1 className="text-2xl font-bold">Perlu Bantuan?</h1>
-                <hr className="w-20 rounded-xl bg-purple-800 border-purple-800 border-y-4" />
-                <p className="text-gray-500">Jangan khawatir cari dan temukan pertanyaanmu disini</p>
+            <section className="flex flex-col gap-y-3 lg:flex-row lg:justify-between lg:items-center lg:w-3/5 lg:self-center">
+                <div className="flex flex-col gap-y-3">
+                    <h1 className="text-2xl font-bold">Perlu Bantuan?</h1>
+                    <hr className="w-20 rounded-xl border-2 border-purple-800 border-y-4" />
+                    <p className="text-gray-500">Jangan khawatir cari dan temukan pertanyaanmu disini</p>
+                </div>
                 <img src="./images/faq-illustration.svg" alt="" />
             </section>
 
-            <section className="flex flex-col gap-y-3">
+            <section className="flex flex-col gap-y-3 lg:w-3/5 lg:self-center md:mb-12">
                 <h1 className="text-2xl font-bold">Pertanyaan Populer</h1>
-                <hr className="w-20 rounded-xl bg-purple-800 border-purple-800 border-y-4" />
+                <hr className="w-20 rounded-xl border-2 border-purple-800 border-y-4" />
 
-                <hr className="border-gray-500" />
-
-                {Faq.map((item) => (
-                    <article className="flex flex-col gap-y-3" key={item.id}>
-                        <div className={`w-full flex justify-between gap-y-3 ${
-                        item.id === 1 ? 'items-center' : ''
-                        }`} onClick={() => toggleIsOpen(item.id)}>
-                            <h2 className={`text-lg font-semibold ${item.id === 1 || item.id === 3 ? 'w-72' : 'w-80'}`}>{item.question}</h2>
-                            {isOpen.includes(item.id) ? (
-                                <i className={`fas fa-chevron-up ${item.id === 1 ? '' : 'mt-2'}`}></i>
-                            ) : (
-                                <i className={`fas fa-chevron-down ${item.id === 1 ? '' : 'mt-2'}`}></i>
-                            )}
-                        </div>
-                        {isOpen.includes(item.id) && (
-                            <div>
-                                <p className="text-gray-500">{item.answer}</p>
-                            </div>
-                        )}
-
+                <section className="lg:flex lg:gap-x-10">
+                    <div className="lg:w-3/4">
                         <hr className="border-gray-500" />
-                    </article>
-                ))}
+                        {Faq.map((item) => (
+                            <article className="flex flex-col gap-y-3" key={item.id}>
+                                <div className={`w-full flex justify-between gap-y-3 lg:mt-3 ${
+                                item.id === 1 ? 'items-center' : ''
+                                }`} onClick={() => toggleIsOpen(item.id)}>
+                                    <h2 className={`text-lg font-semibold ${item.id === 1 || item.id === 3 ? 'w-72 md:w-full' : 'w-80 md:w-full'}`}>{item.question}</h2>
+                                    {isOpen.includes(item.id) ? (
+                                        <i className={`fas fa-chevron-up ${item.id === 1 ? '' : 'mt-2'}`}></i>
+                                    ) : (
+                                        <i className={`fas fa-chevron-down ${item.id === 1 ? '' : 'mt-2'}`}></i>
+                                    )}
+                                </div>
+                                {isOpen.includes(item.id) && (
+                                    <div>
+                                        <p className="text-gray-500">{item.answer}</p>
+                                    </div>
+                                )}
 
-                <section className="flex flex-col gap-y-3">
-                    <button className="bg-white p-5 rounded-xl shadow-md drop-shadow-xl flex gap-x-3 items-center">
-                        <img src="/images/mail.png" alt="Mail" className="w-7" />
-                        <p>Kirim Pesan</p>
-                    </button>
+                                <hr className="border-gray-500" />
+                            </article>
+                        ))}
+                    </div>
 
-                    <button className="bg-white p-5 rounded-xl shadow-md drop-shadow-xl flex gap-x-3 items-center">
-                        <img src="/images/customer.png" alt="Customer Service" className="w-7" />
-                        <p>Customer Service</p>
-                    </button>
+                    <section className="flex flex-col gap-y-3 md:flex-row md:gap-x-4 lg:w-1/4 lg:flex-col lg:items-center lg:justify-center">
+                        <button className="bg-white p-5 rounded-xl shadow-md drop-shadow-xl flex gap-x-3 items-center md:w-1/2 lg:w-full">
+                            <img src="/images/mail.png" alt="Mail" className="w-7" />
+                            <p>Kirim Pesan</p>
+                        </button>
+
+                        <button className="bg-white p-5 rounded-xl shadow-md drop-shadow-xl flex gap-x-3 items-center md:w-1/2 lg:w-full">
+                            <img src="/images/customer.png" alt="Customer Service" className="w-7" />
+                            <p>Customer Service</p>
+                        </button>
+                    </section>
                 </section>
             </section>
         </Fragment>
--- src/components/Layout/AuthLayout.jsx
+++ src/components/Layout/AuthLayout.jsx
@@ -4,68 +4,72 @@ import { Link } from "react-router-dom";
 const AuthLayout = ({ children }) => {
     return (
         <div>
-      <header className='fixed z-10 top-0 w-full h-24 flex items-center justify-between bg-black'>
-        <a href="" className="-ms-2">
-          <img src="./images/logo.png" alt="CodeExpress" className="w-32" />
-        </a>
-        <nav className="text-white me-5">
-            <ul className="flex gap-x-7">
-              <Link to='/'>
-                <li className="hover:text-purple-800">Home</li>
-              </Link>
-              <Link  to='/help'>
-                <li className="hover:text-purple-800">Help</li>
-              </Link>
-            </ul>
-        </nav>
+      <header className='fixed z-10 top-0 w-full bg-black flex justify-center'>
+        <div className="flex items-center justify-between h-24 lg:w-3/5 lg:self-center">
+          <a href="" className="-ms-2">
+            <img src="./images/logo.png" alt="CodeExpress" className="w-32 md:w-36" />
+          </a>
+          <nav className="text-white me-5">
+              <ul className="flex gap-x-7 md:gap-x-10">
+                <Link to='/'>
+                  <li className="hover:text-purple-800 md:text-xl">Home</li>
+                </Link>
+                <Link  to='/help'>
+                  <li className="hover:text-purple-800 md:text-xl">Help</li>
+                </Link>
+              </ul>
+          </nav>
+        </div>
       </header>
 
-      <main className="text-black mt-24 p-3 flex flex-col gap-y-10">
+      <main className="text-black mt-24 p-3 flex flex-col gap-y-10 md:gap-y-20 lg:justify-center">
         {children}
       </main>
 
-      <footer className="bg-black text-white p-3 flex flex-col gap-y-5">
-        <div>
-          <img src="/images/logotext.png" alt="CodeExpress" className="w-52 -mt-20 -ms-10" />
-          <p className="-mt-20">Mari wujudkan impian kalian bersama CodeExpress</p>
-        </div>
-
-        <hr />
-
-        <div className="flex flex-col gap-y-3">
-          <div className="flex flex-col gap-y-1">
-            <h1 className="text-xl font-semibold">Menu Kami</h1>
-            <hr className="w-20 rounded-xl bg-purple-800 border-purple-800 border-y-2" />
+      <footer className="bg-black text-white p-3 flex flex-col gap-y-5 md:text-xl">
+        <section className="flex flex-col gap-y-5 md:flex-row md:p-5 md:justify-center md:items-center md:gap-x-5 lg:w-3/5 lg:self-center">
+          <div className="md:w-1/3">
+            <img src="/images/logotext.png" alt="CodeExpress" className="w-52 -mt-20 -ms-10" />
+            <p className="-mt-20">Mari wujudkan impian kalian bersama CodeExpress</p>
           </div>
-          <ul className="flex flex-col gap-y-2">
-            <li className="hover:text-purple-800">Home</li>
-            <li className="hover:text-purple-800">Help</li>
-          </ul>
-        </div>
 
-        <hr />
+          <hr />
 
-        <div className="flex flex-col gap-y-3">
-          <div className="flex flex-col gap-y-1">
-            <h1 className="text-xl font-semibold">Hubungi Kami</h1>
-            <hr className="w-20 rounded-xl bg-purple-800 border-purple-800 border-y-2" />
+          <div className="flex flex-col gap-y-3 md:w-1/3">
+            <div className="flex flex-col gap-y-1">
+              <h1 className="text-xl font-semibold">Menu Kami</h1>
+              <hr className="w-20 rounded-xl border-2 border-purple-800 border-y-2" />
+            </div>
+            <ul className="flex flex-col gap-y-2">
+              <li className="hover:text-purple-800">Home</li>
+              <li className="hover:text-purple-800">Help</li>
+            </ul>
           </div>
-          <ul className="flex flex-col gap-y-3">
-            <li className="flex gap-x-3 items-center">
-              <img src="/images/email.png" alt="Email" className="w-6" />
-              <p>codeexpress@gmai.com</p>
-            </li>
-            <li className="flex gap-x-3 items-center">
-              <i className="fab fa-whatsapp text-2xl text-green-600 hover:text-purple-800"></i>
-              <p>+62 858 66244363</p>
-            </li>
-          </ul>
-        </div>
 
-        <hr />
+          <hr />
 
-        <div className="text-center flex justify-center items-start">
-          <p className="text-sm">© Copyright 2024. All Rights Reserved by CodeExpress</p>
+          <div className="flex flex-col gap-y-3 md:w-1/3">
+            <div className="flex flex-col gap-y-1">
+              <h1 className="text-xl font-semibold">Hubungi Kami</h1>
+              <hr className="w-20 rounded-xl border-2 border-purple-800 border-y-2" />
+            </div>
+            <ul className="flex flex-col gap-y-3">
+              <li className="flex gap-x-3 items-center">
+                <img src="/images/email.png" alt="Email" className="w-6" />
+                <p>codeexpress@gmai.com</p>
+              </li>
+              <li className="flex gap-x-3 items-center">
+                <i className="fab fa-whatsapp text-2xl text-green-600 hover:text-purple-800"></i>
+                <p>+62 858 66244363</p>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <hr className="lg:w-3/5 lg:self-center" />
+
+        <div className="text-center flex justify-center items-start p-3">
+          <p className="text-sm -mt-4 md:text-xl">© Copyright 2024. All Rights Reserved by CodeExpress</p>
         </div>
       </footer>
     </div>
